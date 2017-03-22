e849124ea47b568d7c558c40623d694735df5d44
Evan Kesten <ebk46@cornell.edu>
2017-03-22 17:52:58 -0400

    algorithm working really well now

---
+++ get_mole_pic.py
@@ -0,0 +1,20 @@
+import json
+import base64
+import sys
+import requests
+from subprocess import call
+
+in_fname = sys.argv[1]
+out_fname = sys.argv[2]
+
+with open(in_fname, 'rb') as in_file:
+    response = requests.post("http://localhost:5000/detect_moles", files={"image": in_file})
+
+json_data = json.loads(response.content)
+mole_file = json_data['mole_file']
+
+with open(out_fname, 'w') as out_file:
+    dec_data = base64.b64decode(mole_file)
+    out_file.write(dec_data)
+
+call(['open', out_fname])
--- mole_detector.py
+++ mole_detector.py
@@ -1,67 +1,32 @@
 import numpy as np
-import scipy as sp
-from scipy.signal import convolve2d
-from scipy.ndimage.filters import gaussian_laplace, generic_filter
-import scipy.stats as st
 import cv2
-from PIL import Image
 import sys
-import math
 
 class MoleDetector:
     def __init__(self, fname):
         self.fname = fname
         self.moles = []
-        self.resized_image = []
-        # right now just use the greyscale image ('L'), use ('1') to use RBG
-        self.raw_image = np.asarray(Image.open(self.fname).convert('L'))
-        self.cv2_raw = cv2.imread(self.fname)
-        self.cv2_small = cv2.resize(self.cv2_raw, (0,0), fx=0.25, fy=0.25)
-
-    def resize_image(self, ratio):
-        new_size = (int(self.raw_image.shape[1]/ratio), int(self.raw_image.shape[0]/ratio))
-        self.resized_image = np.asarray(Image.fromarray(self.raw_image).resize(new_size, Image.ANTIALIAS))
-        #im = Image.fromarray(np.uint8(self.resized_image))
-        #im.show("Ratio = " + str(ratio))
-
-    '''
-    def find_moles(self):
-        max_k = -1
-        max_response = -1
-        threshold = 0
-        for x in range(self.raw_image.shape[1]):
-            for y in range(self.raw_image.shape[0]):
-                for k_size in range(3, 1000):
-    '''
-    def mole_map(self):
-        k_size = 21
-        kernel = np.zeros((k_size, k_size))
-        kernel.fill(1.0/(k_size**2-(k_size-1)**2))
-        kernel[1:-1, 1:-1] = -1.0/((k_size-1)**2)
-        mole_map = convolve2d(self.resized_image, kernel)
-        mole_map /= mole_map.max()
-        im = Image.fromarray(np.uint8(mole_map*255))
-        im.show("Kernel Size = " + str(k_size))
-        return None
-
+        self.image_large = cv2.imread(self.fname)
+        self.image_small = cv2.resize(self.image_large, (0, 0), fx=0.25, fy=0.25)
 
     # explore active contour and major/minor axes for sizing
-    def cv2_mole_map(self):
+    def map_moles(self):
+        image = self.image_large
         params = cv2.SimpleBlobDetector_Params()
-        #params.minThreshold = 100
+        params.minThreshold = 120
         params.filterByArea = True
-        params.minArea = 8
-        params.filterByCircularity = True
-        params.minCircularity = 0.75
-        #params.filterByConvexity = True
-        #params.minConvexity = 1
-        #params.filterByInertia = True
+        params.minArea = 45
+        params.filterByCircularity = False
+        #params.minCircularity = 0.75
+        params.filterByConvexity = True
+        params.minConvexity = .80
+        #params.filterByInertia = False
         #params.minInertiaRatio = 0.20
 
         detector = cv2.SimpleBlobDetector(params)
-        keypoints = detector.detect(self.cv2_small)
+        keypoints = detector.detect(image)
 
-        im_with_moles = cv2.drawKeypoints(self.cv2_small, keypoints, np.array([]), (255,0,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        im_with_moles = cv2.drawKeypoints(image, keypoints, np.array([]), (255, 0, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
         #cv2.imshow("Blob", im_with_moles)
         #cv2.waitKey(0)
@@ -79,8 +44,7 @@ if __name__ == "__main__":
         exit(-1)
 
     md = MoleDetector(sys.argv[1])
-    md.resize_image(10)
-    md.cv2_mole_map()
+    md.map_moles()
 
 
     #im_matrix = im_matrix/im_matrix.max()
@@ -95,4 +59,3 @@ if __name__ == "__main__":
 
     #presumably do some database stuff here
     exit(0)
-
