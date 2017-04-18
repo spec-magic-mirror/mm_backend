fb4a7096243a7da119b379475e24eb46521f9722
Evan Kesten <ebk46@cornell.edu>
2017-04-18 16:23:10 -0400

    db change plus directory restructuring

---
+++ scripts/add_test_dbdata.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+import sys
+import json
+
+clear = False
+for arg in sys.argv:
+    if arg == "--keep" or arg == "-k":
+        clear = True
+
+try:
+    client = MongoClient('localhost', 27017)
+    db = client['MagicMirror']
+except Exception as e:
+    print("Problem with MongoDB: " + e.message)
+
+with open("test_data.json", 'r') as test_file:
+    test_json = json.load(test_file)
+    db.Users.update({"firstName":"Mani", "lastName":"Moles"}, test_json)
