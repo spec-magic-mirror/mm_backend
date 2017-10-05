dd7601cd56e6389c070a24a973a3e1bcb587e77a
Evan Kesten <ebk46@cornell.edu>
2017-10-05 18:53:21 -0400

    made the model generator more memory efficient to run on pi

--- make_rf_model.py
+++ make_rf_model.py
@@ -24,10 +24,29 @@ for path in os.listdir(NEG_SAMPLES):
         neg_paths.append(NEG_SAMPLES + path)
 
 sample_img_shape = cv2.imread(pos_paths[0]).shape
+sis_flat = cv2.imread(pos_paths[0]).flatten().shape
 
-all_x = []
-all_y = []
+num_samples = 0
 
+# to optimize memory, preallocate np arrays
+for img_path in pos_paths:
+    img = cv2.imread(img_path)
+    if img.shape == sample_img_shape:
+        num_samples += 1
+
+for img_path in neg_paths:
+    img = cv2.imread(img_path)
+    if img.shape == sample_img_shape:
+        num_samples += 1
+
+num_samples *= 4
+x_size = (num_samples, sis_flat[0])
+y_size = (num_samples)
+
+all_x = np.zeros(x_size)
+all_y = np.zeros(y_size)
+
+sample_i = 0
 for img_path in pos_paths:
     img = cv2.imread(img_path)
     if img.shape == sample_img_shape:
@@ -35,8 +54,9 @@ for img_path in pos_paths:
         # augment data by rotating 90 degrees 3 times since we have so few positives
         for i in range(rotations):
             img_flattened = img.flatten()
-            all_x.append(img_flattened)
-            all_y.append(1)
+            all_x[sample_i] = img_flattened
+            all_y[sample_i] = 1
+            sample_i += 1
             image_center = tuple(np.array(img.shape)/2)[:2]
             rot_mat = cv2.getRotationMatrix2D(image_center,90, 1.0)
             img = cv2.warpAffine(img, rot_mat, img.shape[:2],flags=cv2.INTER_LINEAR)
@@ -50,23 +70,18 @@ for img_path in neg_paths:
         # augment data by rotating 90 degrees 3 times since we have so few positives
         for i in range(rotations):
             img_flattened = img.flatten()
-            all_x.append(img_flattened)
-            all_y.append(0)
+            all_x[sample_i] = img_flattened
+            all_y[sample_i] = 0
+            sample_i += 1
             image_center = tuple(np.array(img.shape)/2)[:2]
             rot_mat = cv2.getRotationMatrix2D(image_center,90, 1.0)
             img = cv2.warpAffine(img, rot_mat, img.shape[:2],flags=cv2.INTER_LINEAR)
             img[0] = img[1]
             img[:,0] = img[:,1]
 
-all_x = np.array(all_x)
-all_y = np.array(all_y)
-
-train_x = all_x
-train_y = all_y
-
 print("Training Random Forest Classifier...")
 rf_clf = RandomForestClassifier(n_estimators=100, n_jobs=-1)
-rf_clf.fit(train_x, train_y)
+rf_clf.fit(all_x, all_y)
 joblib.dump(rf_clf, 'rf_clf.pkl')
 
 print("Done!")
