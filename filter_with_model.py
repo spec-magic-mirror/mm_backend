e7a45b461ca43c30f238c7879c64405709826ab3
Evan Kesten <ebk46@cornell.edu>
2017-10-11 16:47:58 -0400

    fixed some bugs

--- filter_with_model.py
+++ filter_with_model.py
@@ -1,4 +1,5 @@
 from sklearn.externals import joblib
+from sklearn.externals.joblib import numpy_pickle
 from sklearn.ensemble import RandomForestClassifier
 import sys, os, cv2
 
@@ -9,12 +10,12 @@ if len(sys.argv) != 2:
 TRIAL = sys.argv[1]
 TRIAL_DIR = "trials/" + TRIAL
 
-#rf_clf = joblib.load("rf_clf_32b.pkl")
+rf_clf = joblib.load("rf_clf.pkl")
 results = {0: [], 1:[]}
 raw_paths = os.listdir(TRIAL_DIR)
 for path in raw_paths:
     if not path.startswith("full_face") and path.endswith(".jpg"):
-        img = cv2.imload(path).flatten()
+        img = cv2.imread(TRIAL_DIR + "/" + path).flatten()
         result = rf_clf.predict([img])
         results[result].append(path)
 pprint(results)
