c36ee3e8595fce7bdc4940bd87b3dc4701e64516
Evan Kesten <ebk46@cornell.edu>
2017-10-11 16:22:35 -0400

    recreated filter test for model

---
+++ filter_with_model.py
@@ -0,0 +1,20 @@
+from sklearn.externals import joblib
+from sklearn.ensemble import RandomForestClassifier
+import sys, os, cv2
+
+if len(sys.argv) != 2:
+    print("Run as 'python filter_with_model.py <trial_num>'")
+    exit(0)
+
+TRIAL = sys.argv[1]
+TRIAL_DIR = "trials/" + TRIAL
+
+#rf_clf = joblib.load("rf_clf_32b.pkl")
+results = {0: [], 1:[]}
+raw_paths = os.listdir(TRIAL_DIR)
+for path in raw_paths:
+    if not path.startswith("full_face") and path.endswith(".jpg"):
+        img = cv2.imload(path).flatten()
+        result = rf_clf.predict([img])
+        results[result].append(path)
+pprint(results)
